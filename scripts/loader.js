e129b924f0e6ed57128794bbcf8c1a50b6df23d5
ardbrahaa <ardbr4ha@gmail.com>
2023-10-27 17:06:10 +0200

    Added loader.js loader.css and added the loader to Movies and Shows pages

---
+++ scripts/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  // Hide the preloader when the page is fully loaded
+  const preloader = document.querySelector(".preloader");
+
+  preloader.style.display = "none";
+});
